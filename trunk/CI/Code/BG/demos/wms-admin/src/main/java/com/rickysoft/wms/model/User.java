9f4ca5bf1a7946882c70090ea8fe3e5e59dd163b
mychoice <Jonas-j@outlook.com>
2017-11-15 16:25:19 +0800

    mod=修改

--- trunk/CI/Code/BG/demos/wms-admin/src/main/java/com/rickysoft/wms/model/User.java
+++ trunk/CI/Code/BG/demos/wms-admin/src/main/java/com/rickysoft/wms/model/User.java
@@ -1,17 +1,25 @@
 package com.rickysoft.wms.model;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Table(name = "wms_sys_user")
 public class User {
+    public static final int GENDER_MALE = 1;
+    public static final int GENDER_FEMALE = 2;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String username;
     private String password;
+    private Integer gender;
+    private Integer age;
+    private String mobile;
+    private String email;
+    @Transient
+    private Integer minAge;
+    @Transient
+    private Integer maxAge;
 
     public Integer getId() {
         return id;
@@ -35,5 +43,53 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getMinAge() {
+        return minAge;
+    }
+
+    public void setMinAge(Integer minAge) {
+        this.minAge = minAge;
+    }
+
+    public Integer getMaxAge() {
+        return maxAge;
+    }
+
+    public void setMaxAge(Integer maxAge) {
+        this.maxAge = maxAge;
     }
 }
--- trunk/CI/Code/BG/demos/wms-admin/src/test/java/com/rickysoft/wms/mapper/UserMapperTest.java
+++ trunk/CI/Code/BG/demos/wms-admin/src/test/java/com/rickysoft/wms/mapper/UserMapperTest.java
@@ -1,5 +1,6 @@
 package com.rickysoft.wms.mapper;
 
+import com.github.abel533.entity.Example;
 import com.rickysoft.wms.model.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,8 +17,40 @@ public class UserMapperTest {
     private UserMapper userMapper;
 
     @Test
-    public void testList() {
+    public void testSelect() {
         List<User> users=userMapper.select(new User());
         System.out.println(users.size());
+    }
+
+    @Test
+    public void testSelectByExample() {
+        User condition=new User();
+        condition.setMinAge(28);
+        condition.setMaxAge(32);
+
+        Example example=new Example(User.class);
+        Example.Criteria criteria=example.createCriteria();
+        if(condition.getMinAge() != null) {
+            criteria.andGreaterThan("age",condition.getMinAge());
+        }
+        if(condition.getMaxAge() != null) {
+            criteria.andLessThan("age",condition.getMaxAge());
+        }
+        example.setOrderByClause("id desc");
+
+        List<User> users=userMapper.selectByExample(example);
+        //System.out.println(userMapper.selectCountByExample(example));
+    }
+
+    @Test
+    public void testInsert() {
+        User user=new User();
+        user.setUsername("huangqiang");
+        user.setPassword("123456");
+        user.setGender(User.GENDER_MALE);
+        user.setAge(31);
+        user.setMobile("17666113010");
+        user.setEmail("csuhuangqiang@163.com");
+        userMapper.insert(user);
     }
 }
